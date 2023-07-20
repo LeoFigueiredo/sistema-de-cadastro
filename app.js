abb797b5071bfb5cd47ec9d1c974ceeedbeb1ef8
LeoFigueiredo <leonardofig88@gmail.com>
2023-07-19 21:09:46 -0300

    comentei mais coisa, adicionei uma soma de x e y
    Fiz uma breve conta de adição, e exibi no console.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 console.log("rodando o sistema de gerenciamento de jogos");
 
 console.log("Mais um alerta no console");
+
+console.log ("Modificação adicional do código);
+
+             let x = 10; let y = 2;
+console.log ("Adição de dois valores"x + y);

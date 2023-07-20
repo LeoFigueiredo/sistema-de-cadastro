90c25a2fbd22575761360be37638691ca0049c0b
LeoFigueiredo <leonardofig88@gmail.com>
2023-07-20 17:43:18 -0300

    mudei de ideia sobre o texto em appjs

--- app.js
+++ app.js
@@ -6,3 +6,7 @@ console.log ("Modificação adicional do código);
 
              let x = 10; let y = 2;
 console.log ("Adição de dois valores"x + y);
+
+
+console.log ("mudei de Ideia, adicionando novamente o comentário");
+
